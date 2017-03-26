abe0cbe8098583782c789493c6cc975a9c74073b
udit-khanna <khannaudit@ymail.com>
2017-03-26 17:09:06 +0530

    Excercise completed

--- Excercise_1/src/Excercise/freecharge.java
+++ Excercise_1/src/Excercise/freecharge.java
@@ -1,5 +1,7 @@
 package Excercise;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,6 +16,7 @@ public class freecharge {
 	private static final String CHROMEDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\chromedriver.exe";
 	private static final String INVALID_NUMBER = "1234";
 	private static final String VALID_NUMBER = "9812345678";
+	private static final long TimeOut = 10;
 	private static WebDriver driver;
 
 	public static void main(String[] args) {
@@ -27,8 +30,26 @@ public class freecharge {
 		Enter_number entrNumObj = new Enter_number();
 		entrNumObj.enterNumber(driver, INVALID_NUMBER);
 		entrNumObj.printErrorMessage(driver);
+		freechObj.closeDriver();
+		
+		
+		freechObj.openBrowser("Chrome");
+		freechObj.navigateTo(URL_Site);		
 		entrNumObj.enterNumber(driver, VALID_NUMBER);
-
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		entrNumObj.getDefaultOperator(driver);
+		entrNumObj.getTotalOperators(driver);
+		entrNumObj.selectAnOperator(driver, "IDEA");
+		entrNumObj.clickNextButton(driver);
+		entrNumObj.checkAmountField(driver);
+		entrNumObj.clickViewAllButton(driver);
+		entrNumObj.getTotalRecommendedPlans(driver);
+		entrNumObj.selectEffectivePlan(driver);
 		
 		freechObj.closeDriver();
 
@@ -46,6 +67,7 @@ public class freecharge {
 			} else if (browserType.equalsIgnoreCase("IE")) {
 				driver = new InternetExplorerDriver();
 			}
+			driver.manage().timeouts().implicitlyWait(TimeOut, TimeUnit.SECONDS);				
 			driver.manage().window().maximize();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -56,6 +78,7 @@ public class freecharge {
 		try {
 			System.out.println("Navigating to the site: " + URL);
 			driver.get(URL);
+			driver.manage().timeouts().implicitlyWait(TimeOut, TimeUnit.SECONDS);				
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- Excercise_1/src/Runfiles/Enter_number.java
+++ Excercise_1/src/Runfiles/Enter_number.java
@@ -1,16 +1,28 @@
 package Runfiles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class Enter_number {
 	private static final String inputPhoneNumber = "//*[@class='input-container _3udlq _22ixJ undefined']/input";
 	private static final String phoneNumberMessage = "//*[@id='op_loader']/following-sibling::span[2]";
 	private static final String inputPhoneNumber_NumEntered = "//*[@class='input-container _3udlq _22ixJ error']/input";
 	private static final String phoneNumType = "//*[@name='dataCardType']/parent::label/span[1]";
-	
-	
+	private static final String proceedButton = "//*[@class='btn _2eaVn']";
+	private static final String operatorDropdown = "//*[@name='operator']";
+	private static final String operatorDropdown_options = "//*[@name='operator']/option";
+	private static final String circleDropdown = "//*[@name='circle']/option";
+	private static final String amountField = "//*[@name='amount']";
+	private static final String viewAllButton = "//*[@class='_3G6A5']";
+	private static final String recommendedPlans = "//*[@class='_3Ek1g']/ul/li";
+	private static final String recommendedPlans_Details = "//*[@class='_3Ek1g']/ul/li/div/p[2]";
+
 	public void enterNumber(WebDriver driver, String data) {
 		try {
 			driver.findElement(By.xpath(inputPhoneNumber)).sendKeys(data);
@@ -36,22 +48,123 @@ public class Enter_number {
 			e.printStackTrace();
 		}
 	}
-	
-	public void hoverOver(WebDriver driver, String element){
-		try{
+
+	public void hoverOver(WebDriver driver, String element) {
+		try {
 			System.out.println("Hovering over the element");
 			Actions action = new Actions(driver);
 			action.moveToElement(driver.findElement(By.xpath(element))).click().build().perform();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	public void checkPhoneNumType(WebDriver driver){
-		try {
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void clearTextField(WebDriver driver) {
+		try {
+			driver.findElement(By.xpath(inputPhoneNumber)).clear();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// public void checkPhoneNumType(WebDriver driver){
+	// try {
+	//
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	public void clickButton(WebDriver driver, String xPath) {
+		try {
+			driver.findElement(By.xpath(xPath)).click();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void getDefaultOperator(WebDriver driver) {
+		try {
+			// String currentOperator =
+			// driver.findElement(By.xpath(operatorDropdown)).getText();
+			Select currentOperator = new Select(driver.findElement(By.xpath(operatorDropdown)));
+			String selectedValue = currentOperator.getFirstSelectedOption().getText();
+			System.out.println("Current operator is:" + selectedValue);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void getTotalOperators(WebDriver driver) {
+		try {
+			System.out.println(
+					"Total number of Operators: " + driver.findElements(By.xpath(operatorDropdown_options)).size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void selectAnOperator(WebDriver driver, String value) {
+		try {
+			List<WebElement> operatorTypes = new ArrayList<WebElement>();
+			operatorTypes = driver.findElements(By.xpath(operatorDropdown_options));
+			clickButton(driver, operatorDropdown);
+			for (WebElement w : operatorTypes) {
+				if (w.getText().equalsIgnoreCase(value))
+					w.click();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void clickNextButton(WebDriver driver) {
+		try {
+			clickButton(driver, proceedButton);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void checkAmountField(WebDriver driver) {
+		try {
+			if (driver.findElement(By.xpath(amountField)).getText().equalsIgnoreCase(null)) {
+				System.out.println("Amount field is empty");
+			} else {
+				System.out.println("Amount field is not empty");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
+	}
+
+	public void clickViewAllButton(WebDriver driver) {
+		clickButton(driver, viewAllButton);
+	}
+
+	public void getTotalRecommendedPlans(WebDriver driver) {
+		try {
+			List<WebElement> totalPlans = new ArrayList<WebElement>();
+			totalPlans = driver.findElements(By.xpath(recommendedPlans));
+			System.out.println("Total Recommended Plans are: " + totalPlans.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void selectEffectivePlan(WebDriver driver) {
+		try {
+			List<WebElement> totalPlans = new ArrayList<WebElement>();
+			totalPlans = driver.findElements(By.xpath(recommendedPlans_Details));
+			for(WebElement w: totalPlans){
+				if(w.getText().contains("Full Talktime")){
+					w.click();
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
