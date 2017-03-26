66951a865ac63e59e3162e984d4a941325a812a3
udit-khanna <khannaudit@ymail.com>
2017-03-26 22:09:51 +0530

    Created Exercise 2 project

---
+++ Exercise-2/src/executable/ExecutableFile.java
@@ -0,0 +1,63 @@
+package executable;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class ExecutableFile {
+	private static final String FIREFOXDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\geckodriver.exe";
+	private static final String CHROMEDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\chromedriver.exe";
+	private static final String IEDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\IEDriverServer.exe";
+	private static final long TimeOut = 10;
+	private static WebDriver driver;
+
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public void openBrowser(String browserType) {
+		try {
+			System.out.println("Opening Browser: " + browserType);
+			if (browserType.equalsIgnoreCase("firefox")) {
+				System.setProperty("webdriver.gecko.driver", FIREFOXDRIVERPATH);
+				driver = new FirefoxDriver();
+			} else if (browserType.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver", CHROMEDRIVERPATH);
+				driver = new ChromeDriver();
+			} else if (browserType.equalsIgnoreCase("IE")) {
+				driver = new InternetExplorerDriver();
+			}
+			driver.manage().timeouts().implicitlyWait(TimeOut, TimeUnit.SECONDS);				
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void navigateTo(String URL) {
+		try {
+			System.out.println("Navigating to the site: " + URL);
+			driver.get(URL);
+			driver.manage().timeouts().implicitlyWait(TimeOut, TimeUnit.SECONDS);				
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void closeDriver() {
+		try {
+			System.out.println("Closing the browser.");
+			driver.quit();
+			System.out.println("Browser closed");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
