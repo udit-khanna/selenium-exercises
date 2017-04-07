3534e93ea7b3d524b3b6c3a649e19bb899c07426
udit-khanna <khannaudit@ymail.com>
2017-04-07 11:23:29 +0530

    Created Base structure for the exercise

---
+++ Exercise-3/src/executableFile/ExecutableFile.java
@@ -0,0 +1,71 @@
+package executableFile;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import keywords.Keywords;
+
+public class ExecutableFile {
+	private static final String FIREFOXDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\geckodriver.exe";
+	private static final String CHROMEDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\chromedriver.exe";
+	private static final String IEDRIVERPATH = System.getProperty("user.dir") + "\\Support_Files\\IEDriverServer.exe";
+	private static final String URL = "https://www.freecharge.in/";
+	private static final long TimeOut = 10;
+	private static WebDriver driver;
+
+	public static void main(String[] args) {
+		ExecutableFile exe = new ExecutableFile();
+		Keywords keyword = new Keywords();
+
+		exe.openBrowser("Chrome");
+		exe.navigateTo(URL);
+		exe.closeDriver();
+
+	}
+
+	public void openBrowser(String browserType) {
+		try {
+			System.out.println("Opening Browser: " + browserType);
+			if (browserType.equalsIgnoreCase("firefox")) {
+				System.setProperty("webdriver.gecko.driver", FIREFOXDRIVERPATH);
+				driver = new FirefoxDriver();
+			} else if (browserType.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver", CHROMEDRIVERPATH);
+				driver = new ChromeDriver();
+			} else if (browserType.equalsIgnoreCase("IE")) {
+				driver = new InternetExplorerDriver();
+			}
+			driver.manage().timeouts().implicitlyWait(TimeOut, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void navigateTo(String URL) {
+		try {
+			System.out.println("Navigating to the site: " + URL);
+			driver.get(URL);
+			driver.manage().timeouts().implicitlyWait(TimeOut, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void closeDriver() {
+		try {
+			System.out.println("Closing the browser.");
+			driver.quit();
+			System.out.println("Browser closed");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
--- README.md
+++ README.md
@@ -6,16 +6,16 @@ This repository contains exercises related to Selenium webdriver
 
 * Navigate to https://www.freecharge.in/
 * Reach VIEW PLANS sidebar after entering the number and selecting the recharge type and the operator (Select operator as Airtel)
-* Find the total number of RECOMMENDED plan
-* Find the most effective RECOMMENDED plans
-  * Verify the existence FULL TALKTIME plan with any other options
+* Find the total number of **RECOMMENDED** plan
+* Find the most effective **RECOMMENDED** plans
+  * Verify the existence **FULL TALKTIME** plan with any other options
   * Verify the existence of any other combo plans
   * Verify the existence of any sms plans
-* Navigate to the FULL TALKTIME tab
+* Navigate to the **FULL TALKTIME** tab
   * Check the existence of any other plans rather than full talk time. Fail the test case in case any
-* Navigate to the TOP UP tab
+* Navigate to the **TOP UP** tab
   * Find the plan with least validity 
-* Navigate to the OTHER tab
-  * Find total number of ISD plans
+* Navigate to the **OTHER** tab
+  * Find total number of **ISD** plans
 
 - - - -
